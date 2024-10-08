b89ead0bcdc4879262013cb8d39124fdb8739380
GanaF4ll <ganafall9498@gmail.com>
2024-10-08 12:07:09 +0200

    🎉 first commit

---
+++ src/api/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://mongo:27017/mydatabase")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}`);
+});
