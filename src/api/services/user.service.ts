1872f8a1b0d36a47ee4058888c602e73561de063
GanaF4ll <ganafall9498@gmail.com>
2024-10-08 14:14:56 +0200

    ➕ bcrypt + jwt dependencies

--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -1,1 +1,7 @@
-export class UserService {}
+import { User } from "../models/user.model";
+import { bcrypt } from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export class UserService {
+  async register() {}
+}
