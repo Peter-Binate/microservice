57dd5d7b6a2c61ca35826309e47b7f30080d581a
GanaF4ll <ganafall9498@gmail.com>
2024-10-08 14:40:40 +0200

    model update + service methods

--- src/api/models/user.model.ts
+++ src/api/models/user.model.ts
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  role: boolean;
+}
 
 const userSchema = new Schema({
-  email: { type: String, required: "true" },
-  password: { type: String, required: "true" },
-  role: { type: Boolean, required: "true", default: 1 },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: Boolean, required: true, default: true },
 });
 
-export const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export { User };
--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -1,7 +1,47 @@
-import { User } from "../models/user.model";
-import { bcrypt } from "bcrypt";
+import { User, IUser } from "../models/user.model";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { CreateUserDto } from "../dtos/user.dto";
 
 export class UserService {
-  async register() {}
+  async register(createUserDto: CreateUserDto): Promise<IUser> {
+    const { email, password, role } = createUserDto;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new Error(`Email: ${email} already taken`);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      role: role !== undefined ? role : true,
+    });
+
+    await newUser.save();
+
+    return newUser;
+  }
+
+  async login(email: string, password: string): Promise<string> {
+    const existingUser = await User.findOne({ email });
+
+    if (
+      !existingUser ||
+      !(await bcrypt.compare(password, existingUser.password))
+    ) {
+      throw new Error("Incorrect email or password");
+    }
+
+    const payload = { id: existingUser._id, email: existingUser.email };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+      expiresIn: "48h",
+    });
+
+    return token;
+  }
 }
