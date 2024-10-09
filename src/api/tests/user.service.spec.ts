48949c52d8de158cd9520c4c112f144f3d5d7fc8
GanaF4ll <ganafall9498@gmail.com>
2024-10-09 12:35:20 +0200

    ➕ 🔧 jest config

---
+++ src/api/tests/user.service.spec.ts
@@ -0,0 +1,3 @@
+import { UserService } from "./../services/user.service";
+import { User, IUser } from "../models/user.model";
+import { CreateUserDto, UpdateUserDto, LoginDto } from "../dtos/user.dto";
